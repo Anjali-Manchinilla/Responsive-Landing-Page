f86932f06e5ee0450a63f7e64497eefe47eb4968
Anjali-Manchinilla <manchinillaanjali822@gmail.com>
2026-06-16 17:00:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Select navbar
+const navbar = document.getElementById("navbar");
+
+// Change navbar style on scroll
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
